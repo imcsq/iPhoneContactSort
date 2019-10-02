49ba8be9709ee0c7fa72aa4c963334bb6a67664c
imcsq <imcsq@qq.com>
2019-10-02 23:17:39 +0800

    添加功能：中文姓名规范化（拆分姓/名） & 为姓和名均添加拼音注音

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 iphone-contact-sort.py
 =========
 
-为iPhone通讯录中文加入拼音排序，适用于英文界面的中文通讯录排序，支持.vcf格式
+为iPhone通讯录中文名规范姓名（默认第一个字为姓，其余为名）并加入姓名拼音排序，适用于英文界面的中文通讯录条目排序
+
+输入原始.vcf格式通讯录文件，该文件可从iCloud导出，运行程序后输出格式化后的.vcf，随后导回iCloud即可。
 
 Example:
 
@@ -12,3 +14,4 @@ Example:
 
 转换成功后，使用iCloud导入新的通讯录文件即可
 
+Current Runtime: Python 2
--- iPhoneContactSort.py
+++ iPhoneContactSort.py
@@ -1,7 +1,3 @@
-"""
-    Author:zheng.li
-    E-mail:ronnie.alonso@gmail.com
-"""
 import re
 from pinyin import PinYin
 
@@ -17,10 +13,17 @@ class iPhoneContactSort(object):
         f = open(self.filename,'r')
         for line in open(self.filename):  
             line = f.readline()
-            contact.append(line)
-            k = re.findall(r"(\N\:[^\;]*\;)", line) 
+            k = re.findall(r"(\N\:[^\;]*\;[^\;]*\;[^\;]*\;[^\;]*\;)", line) 
             if k:
-                phones = py_engine.hanzi2pinyin(string=k[0])
+                if k[0].find(';') - 2 > 3:
+                    xing = k[0][2: 5]
+                    ming = k[0][5: k[0].find(';')] + k[0][k[0].find(';') + 1 : k[0].find(';', k[0].find(';')+1)]
+                else:
+                    xing = k[0][2: k[0].find(';') ]
+                    ming = k[0][k[0].find(';') + 1 : k[0].find(';', k[0].find(';')+1)]
+                contact.append('N:'+xing+';'+ming+';'+";;\n")
+
+                phones = py_engine.hanzi2pinyin(string=xing)
                 line = "X-PHONETIC-LAST-NAME:"
                 for item in phones:
                     if item != '':
@@ -28,11 +31,21 @@ class iPhoneContactSort(object):
                 line += "\n"
                 contact.append(line)
 
-        fout = open(filename, 'w')
+                phones = py_engine.hanzi2pinyin(string=ming)
+                line = "X-PHONETIC-FIRST-NAME:"
+                for item in phones:
+                    if item != '':
+                        line = line + item.capitalize()
+                line += "\n"
+                contact.append(line)
+            else:
+                contact.append(line)
+
+        fout = open("ok_"+self.filename, 'w')
         for line in contact:
             fout.write(line)
 
 if __name__ == '__main__':
-    filename = 'Brown David and 397 others.vcf'
+    filename = 'a.vcf'
     i_engine = iPhoneContactSort(filename)
     i_engine.Convert()
